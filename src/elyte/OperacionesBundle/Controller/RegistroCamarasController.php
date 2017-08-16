625b918c6dae8bbf9a7c491c6111cb58d8822be1
uno <tecladozurdo@mail.com>
2017-08-15 22:44:13 -0500

    feat: modificacion de la presentacion del historial

--- src/elyte/OperacionesBundle/Controller/RegistroCamarasController.php
+++ src/elyte/OperacionesBundle/Controller/RegistroCamarasController.php
@@ -4,22 +4,36 @@ namespace elyte\OperacionesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Utilitarios\UtilBundle\Controller\Repositorios;
 use BD\DaoBundle\Entity\Camaras;
 use BD\DaoBundle\Form\CamarasType;
 
 class RegistroCamarasController extends Controller {
+
   /**
   *Metodo inicial del
   *@Route("/regCamaras",name="_regCamaras")
   */
-  public function indexAction(){
+  public function camaraAction(){
 
-  	$camaras = new Camaras();
-
-  	$form = $this->createForm(CamarasType::class,$camaras);
-
-
-    return $this->render("OperacionesBundle:Registro:camaras.html.twig", array('form'=>$form->createView()));
+  	//$camaras = new Camaras();
+  	//$form = $this->createForm(CamarasType::class,$camaras);
+    //return $this->render("OperacionesBundle:Registro:camaras.html.twig", array('form'=>$form->createView()));
+    return $this->render("OperacionesBundle:Registro:camaras.html.twig");
   }
+
+  /**
+  *Metodo que dado un codigo retorna el historial de camaras activas e inactivas
+  *@Route("/getCamara/{codigo}",name="_getCamara",defaults={"codigo"=null})
+  */
+  public function getCamaraAction($codigo){
+	  $em = $this->getDoctrine()->getManager();
+	  $repositorio = $em->getRepository(Repositorios::$camaras);
+	  $list = $repositorio->findBy(array('codigo'=>$codigo));
+	  
+  	return new JsonResponse($list);
+  }
+
 }
